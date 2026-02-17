8eb88c07984da32b140c32ac1ac03b427a897b67
Angelo Diaz <diazreyesangelo@gmail.com>
2026-02-16 22:48:42 -0500

    primer commit

---
+++ practicas/EstructurasControl.py
@@ -0,0 +1,21 @@
+""" Ejercicio
+Temperatura = 22
+if Temperatura > 20 :
+    print ("hace calor")
+    """
+
+""" Ejercicio
+Temperatura   = int (input("Ingrese la temperatura: "))
+if Temperatura > 20 :
+    print ("hace calor")
+"""
+""" Ejercicio
+Temperatura   = int (input("Ingrese la temperatura: "))
+if Temperatura > 16 and Temperatura < 22 :
+    print ("El clima es bueno")
+"""
+
+Amigos = ["juan","Diego","angelo" ]
+Nombre = input (" dime tu nombre ")
+if Nombre in Amigos:
+    print (f"{Nombre} esta en mi rupo de amigos")
---
+++ practicas/booleanos.py
@@ -0,0 +1,46 @@
+""" Ejercicio
+a = True 
+print (type(a))
+
+
+x = 9
+y = 8
+Comparar = x == y
+print (Comparar)
+
+Comparar2 = x > y
+print (Comparar2)
+
+Z = 8 
+Comparar3 = x < Z and y == Z
+print (Comparar3)
+
+Comparar4 = x > Z or y == Z
+print (Comparar4)
+
+Comparar5 =  x != y
+print (Comparar5)
+"""
+
+"""
+Num1 = 10
+Num2 = 5
+
+Comparacion = Num1 == Num2
+print (Comparacion)
+
+condicion1 = True
+condicion2 = False
+condicion3 = True
+
+resultado = (condicion1 and condicion3) or condicion2
+
+print(resultado)
+"""
+
+A = 8
+B = 6
+C = 4
+
+Resultado = A > B or B < C
+print (Resultado)
